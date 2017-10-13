2171b37bbf59efef6bda47bdc0f6420b38f4653c
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2017-10-13 18:25:12 +0200

    minor fixes

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <div class="carousel-inner">
       <?php while ( $slider_loop->have_posts() ) : $slider_loop->the_post();?>
         <div class="carousel-item">
-          <img class="d-block w-100" src="<?php the_post_thumbnail_url(); ?>" alt="Luxtime - watches">
+          <img class="d-flex w-100" src="<?php the_post_thumbnail_url(); ?>" alt="Luxtime - watches">
           <div class="carousel-caption d-none d-md-block">
             <h3><?php the_title(); ?></h3>
             <p><?php the_content(); ?></p>
