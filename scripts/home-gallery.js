160c5350e7117a78773849566f942cdc9926ba79
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2017-10-28 14:01:27 +0200

    Ordering script fix

--- scripts/home-gallery.js
+++ scripts/home-gallery.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
-    let newestProducts = document.querySelector('.latest-products').children;
-    $.each(newestProducts, function(i, elem) {
+    let catalogProducts = document.querySelector('.catalog-products').children;
+    $.each(catalogProducts, function(i, elem) {
       elem.classList.add('col-12', 'col-sm-12', 'col-md-6', 'col-lg-4');
   });
 });
