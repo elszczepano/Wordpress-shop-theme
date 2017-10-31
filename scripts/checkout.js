ae26021a231b802081c79d33caed9f5deeb55c91
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2017-10-31 16:38:50 +0100

    Checkout page style and scripts

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,3 +1,5 @@
-let checkoutFields = document.querySelector('.col-1');
-checkoutFields.classList.add('col-10', 'col-sm-10', 'col-md-7', 'col-lg-7');
+const checkoutFields = document.querySelector('.col-1');
+checkoutFields.classList.add('col-10', 'col-sm-10', 'col-md-8', 'col-lg-8');
 checkoutFields.classList.remove('col-1');
+document.querySelector('.col-2').remove();
+$('p.form-row input').addClass(' form-control ');
