13c4c51b6fc9a63ea0f0b1f79d994f10da3ab04b
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2017-11-02 19:29:36 +0100

    Login page CSS's

--- page-moje-konto.php
+++ page-moje-konto.php
@@ -1,7 +1,10 @@
 <?php
 get_header();
-    if (have_posts()) : while (have_posts()) : the_post();
-      the_content();
+    if (have_posts()) : while (have_posts()) : the_post();?>
+      <div class="login-container">
+        <?php the_content(); ?>
+      </div>
+    <?php
     endwhile;
     endif;
 get_footer();
