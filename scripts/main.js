bc4e8afa17223f0966ddf42f0c33c91c7bbf0ff9
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2017-11-05 17:46:18 +0100

    Contact forms CSS and scripts

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,16 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
+if(document.querySelector( '.wpcf7-form' )!=null) {
+  document.querySelector('.wpcf7').classList.add('d-flex' , 'justify-content-center');
+  if(document.querySelectorAll( 'input' )!=null) {
+  const inputs = document.querySelectorAll( 'input' );
+      $.each(inputs, function(i, elem) {
+        elem.classList.add('form-control');
+  });
+  }
+  const textareas = document.querySelectorAll( 'textarea' );
+      $.each(textareas, function(i, elem) {
+        elem.classList.add('form-control');
+  });
+}
