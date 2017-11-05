d3aff9143aa73d7f48563b6e3a8718fe7087300c
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2017-11-05 17:02:25 +0100

    Fix bugs of reading null selectors in catalog

--- scripts/category.js
+++ scripts/category.js
@@ -1,4 +1,3 @@
-
 document.querySelector('.woocommerce-breadcrumb').setAttribute('aria-label' , 'breadcrumb');
 document.querySelector('.woocommerce-breadcrumb').setAttribute('role' , 'navigation');
 document.querySelector('.woocommerce-breadcrumb').classList.add('breadcrumb');
@@ -6,5 +5,9 @@ document.querySelector('.woocommerce-breadcrumb a').classList.add('breadcrumb-it
 document.querySelector('#sidebar').remove();
 document.querySelector('#content').classList.add('catalog-products-box');
 document.querySelector('.woocommerce-products-header').classList.add('catalog-products-header');
-document.querySelector('.products').classList.add('catalog-products' , 'd-flex' , 'flex-wrap');
-document.querySelector('.orderby').classList.add('form-control');
+if(document.querySelector('.products')!=null) {
+  document.querySelector('.products').classList.add('catalog-products' , 'd-flex' , 'flex-wrap');
+}
+if(document.querySelector('.orderby')!=null) {
+  document.querySelector('.orderby').classList.add('form-control');
+}
