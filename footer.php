e4fbd5091b90d48f9e71e4d24c022e6a2d7dc91e
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2017-11-11 16:27:41 +0100

    Responsive footer

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
 </main>
 <footer class="d-flex flex-row main-footer">
   <div class="container justify-content-center">
-    <div class=" d-flex flex-row justify-content-center">
-      <div>
+    <div class=" d-flex flex-row justify-content-center flex-wrap">
+      <div class="col-6 col-lg-3">
         <h4>Kategorie:</h4>
         <ul>
           <?php
@@ -20,7 +20,7 @@
           <?php endforeach;?>
         </ul>
       </div>
-      <div>
+      <div class="col-6 col-lg-3">
         <h4>O firmie:</h4>
         <ul>
           <li><a href="<?php echo get_template_directory_uri() . '/nasze-sklepy' ?>">Nasze sklepy</a></li>
@@ -29,14 +29,14 @@
           <li><a href="<?php echo get_template_directory_uri() . '/kontakt' ?>">Kontakt</a></li>
         </ul>
       </div>
-      <div>
+      <div class="col-6 col-lg-3">
         <h4>Serwis:</h4>
         <ul>
           <li><a href="<?php echo get_template_directory_uri() . '/obsluga-klienta' ?>">Dział obsługi klienta</a></li>
           <li><a href="<?php echo get_template_directory_uri() . '/reklamacje' ?>">Zwroty i reklamacje</a></li>
         </ul>
       </div>
-      <div>
+      <div class="col-6 col-lg-3">
         <h4>Kontakt:</h4>
         <ul>
           <li><strong>+48 784 342 533</strong></li>
