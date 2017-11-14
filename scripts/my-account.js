c8765544dbb5f8ccb293c5bfefb0f408d00825ce
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2017-11-14 18:04:17 +0100

    Responsive login page

--- scripts/my-account.js
+++ scripts/my-account.js
@@ -1,7 +1,9 @@
 const loginFields = document.querySelector('.col-1');
 const registerFields = document.querySelector('.col-2');
+const accountBox = document.querySelector('.col2-set');
 
-if(loginFields!=null&&registerFields!=null) {
+if(loginFields!=null&&registerFields!=null&&accountBox!=null) {
+  accountBox.classList.add('col-12' , 'd-flex' , 'flex-wrap');
   loginFields.classList.add('col-12' , 'col-sm-12' , 'col-md-5' , 'col-lg-5');
   loginFields.classList.remove('col-1');
   registerFields.classList.add('col-12' , 'col-sm-12' , 'col-md-5' , 'col-lg-5');
