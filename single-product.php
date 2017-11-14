251f0a1623d0154c509f04ce281d91d99f2ea908
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2017-11-14 19:38:52 +0100

    Single product responsive page

--- single-product.php
+++ single-product.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 <?php do_action( 'woocommerce_before_single_product' ); ?>
-<div class="product-container row">
-      <div class="col owl-carousel product-gallery-carousel">
+<div class="product-container row d-flex flex-wrap">
+      <div class="col-12 col-lg-6 owl-carousel product-gallery-carousel">
         <?php
         global $product;
         $attachment_ids = $product->get_gallery_attachment_ids();
@@ -9,7 +9,7 @@
             <div class="product-gallery-item" style="background-image:url('<?php echo wp_get_attachment_url( $attachment_id ); ?>')"></div>
         <?php endforeach; ?>
     </div>
-    <div class="col product-about">
+    <div class="col-12 col-lg-6 product-about">
       <?php do_action('woocommerce_single_product_summary'); ?>
       <?php if($product->get_stock_quantity()!=NULL): ?>
       <span class="stock">Na magazynie: <span class="stock-quantity"><?php echo $product->get_stock_quantity(); ?></span> szt.</span>
